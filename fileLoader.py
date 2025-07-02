d7dcf21c63041b9812d1b44fd9b83ac32cc732a2
Tomas <t.sierras@uniandes.edu.co>
2025-07-02 18:51:19 -0500

    Question

--- fileLoader.py
+++ fileLoader.py
@@ -1,9 +1,11 @@
-from fastapi import FastAPI, File, UploadFile
+from fastapi import Body, FastAPI, File, UploadFile
 import requests
 
 app = FastAPI()
 
 urlFiles = "https://n8n.virtual.uniandes.edu.co/webhook/fba4005f-cf21-4bcb-b29d-4234835afbf5"
+urlQuery = "https://n8n.virtual.uniandes.edu.co/webhook/509ac137-da63-400f-93fd-e247a7cb3971"
+
 
 @app.post("/upload/file")
 async def upload_file(file: UploadFile = File(...)):
@@ -23,19 +25,15 @@ async def upload_file(file: UploadFile = File(...)):
     }
 
 @app.post("/retrieve/question")
-async def upload_file():
-    # Read the file content
-    contents = await file.read()
+def receive_string(data: str = Body(..., embed=True)):
 
-    # Send it using requests
     response = requests.post(
-        url=urlFiles,
-        files={"file": (file.filename, contents, file.content_type)}
+        url=urlQuery,
+        data={"query": data}
     )
+    
+    responseJson = response.json()
 
-    return {
-        "message": "File sent",
-        "status_code": response.status_code,
-        "response_text": response.text
-    }
+    print(responseJson["output"])
 
+    return {"received": responseJson["output"]}
